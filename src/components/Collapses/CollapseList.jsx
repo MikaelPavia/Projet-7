fbf29ccdbcdee5af44321c863a22d41f427f8c55
MikaelPavia <101260071+MikaelPavia@users.noreply.github.com>
2022-11-09 12:19:48 +0100

    Remove Styled Components in CollapseList

--- src/components/Collapses/CollapseList.jsx
+++ src/components/Collapses/CollapseList.jsx
@@ -1,4 +1,3 @@
-import styled from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
@@ -7,47 +6,9 @@ import './Collapse.scss'
 const ChevronIcon = <FontAwesomeIcon icon={faChevronUp}/>
 
 
-
-
-const Icon = styled.div`
-font-size: 30px;
-transform: rotate(180deg);
-margin-right: 10px;
-transition: transform 500ms;
-    ${(props) =>
-    props.isOpen &&
-    `
-    transform: rotate(0deg);`}
-`
-
-
-
-const Description = styled.div`
-height: auto;
-width: 100%;
-font-size: 18px;
-background-color: #f2f2f2;
-border-radius: 10px;
-margin-top: -30px;
-color: #EF6C6C;
-transform-origin: 50% top;
-transform: scaleY(0);
-transition: transform 500ms;
-position: relative;
-z-index: -10;
-    ${(props) =>
-    props.isOpen &&
-    `
-    transform: scaleY(1)`}
-
-`
-
-
-
-
-function CollapseList ({title, list}){  // passer en props le titre et la description, puis mettre contenu directement dans collapse about
+function CollapseList ({title, list}){  
     
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
     
     const { id } = useParams()
     
@@ -63,19 +24,19 @@ function CollapseList ({title, list}){  // passer en props le titre et la descri
 
                     <div className="titleCollapse">{title}</div>
 
-                        <Icon isOpen={isOpen} onClick={() => {
+                        <div isOpen={isOpen} onClick={() => {
 
                             setIsOpen(!isOpen)
 
-                        }}>{ChevronIcon}</Icon>
+                        }} className={ isOpen ? 'IconCollapse' : 'IconCollapse IconCollapseOpened'}>{ChevronIcon}</div>
 
                 </div>
 
             </div>
 
-            <Description isOpen={isOpen}>
+            <div isOpen={isOpen}>
 
-                <div>
+                <div className={isOpen ? 'DescriptionCollapse' : 'DescriptionCollapse DescriptionCollapseOpened'}>
 
                     <ul className="ulCollapse">
 
@@ -90,7 +51,7 @@ function CollapseList ({title, list}){  // passer en props le titre et la descri
                 </div>
 
 
-            </Description>
+            </div>
 
         </div>
 
